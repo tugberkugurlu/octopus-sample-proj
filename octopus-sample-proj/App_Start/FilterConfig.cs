32087bb86ee2e2ecdf88bc08ce7ff38ad75a0e14
unknown <tugberk@outlook.com>
2015-01-16 13:09:39 +0000

    initial load

---
+++ octopus-sample-proj/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace octopus_sample_proj
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ octopus-sample-proj/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="octopus_sample_proj.MvcApplication" Language="C#" %>
